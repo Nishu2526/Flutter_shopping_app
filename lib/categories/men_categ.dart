f897009cf00358e07ea72fe384b384f27ff3a2d4
Nishu2526 <nishikamartin2526@gmail.com>
2022-10-07 12:34:44 +0530

    This is Shopping App..

--- lib/categories/men_categ.dart
+++ lib/categories/men_categ.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:bottomnavigation/utilies/cate_list.dart';
-
 import '../mainor_screen/subcate_products.dart';
 
 class MenCategory extends StatelessWidget {
@@ -8,49 +7,102 @@ class MenCategory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return Stack(
       children: [
-        const Padding(
-          padding: EdgeInsets.all(30),
-          child: Text(
-            'Men',
-            style: TextStyle(
-                fontSize: 25, fontWeight: FontWeight.bold, letterSpacing: 2),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.8,
+            width: MediaQuery.of(context).size.width * 0.75,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Text(
+                    'Men',
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 2),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.68,
+                  child: GridView.count(
+                      crossAxisCount: 3,
+                      mainAxisSpacing: 40,
+                      // crossAxisSpacing: 20,
+                      children: List.generate(men.length, (index) {
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SubCategoryScreen(
+                                          subcategName: men[index],
+                                          mainCategName: 'Men',
+                                        )));
+                          },
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Container(
+                                    color: Colors.grey.shade400,
+                                    height: 100,
+                                    width: 100,
+                                    child: Image(
+                                      image: AssetImage(
+                                          'images/men/men$index.jpg'),
+                                    )),
+                                Text(men[index])
+                              ]),
+                        );
+                      })),
+                ),
+              ],
+            ),
           ),
         ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.68,
-          child: GridView.count(
-              crossAxisCount: 3,
-              mainAxisSpacing: 40,
-              // crossAxisSpacing: 20,
-              children: List.generate(men.length, (index) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => SubCategoryScreen(
-                                  subcategName: men[index],
-                                  mainCategName: 'Men',
-                                )));
-                  },
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Container(
-                            color: Colors.grey.shade400,
-                            height: 100,
-                            width: 100,
-                            child: Image(
-                              image: AssetImage('images/men/men$index.jpg'),
-                            )),
-                        Text(men[index])
-                      ]),
-                );
-              })),
-        ),
+        Positioned(
+            bottom: 0,
+            right: 0,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5.0),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.8,
+                width: MediaQuery.of(context).size.width * 0.05,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 40),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: RotatedBox(
+                      quarterTurns: 3,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          const Text(
+                            ' << ',
+                            style: TextStyle(fontSize: 16, letterSpacing: 10),
+                          ),
+                          Text(
+                            'men'.toUpperCase(),
+                            style: const TextStyle(fontSize: 16, letterSpacing: 10),
+                          ),
+                          const Text(
+                            ' >> ',
+                            style: TextStyle(fontSize: 16, letterSpacing: 10),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ))
       ],
     );
   }
--- lib/mainor_screen/subcate_products.dart
+++ lib/mainor_screen/subcate_products.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 class SubCategoryScreen extends StatelessWidget {
   final String mainCategName;
   final String subcategName;
-  const SubCategoryScreen({Key? key, required this.subcategName, required this.mainCategName, }) : super(key: key);
+  const SubCategoryScreen({
+    Key? key,
+    required this.subcategName,
+    required this.mainCategName,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,14 +15,27 @@ class SubCategoryScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        leading: IconButton(icon: const Icon(Icons.arrow_back_ios_new, color: Colors.black,), onPressed: () {
-          Navigator.pop(context);
-        },),
-        title: Text(subcategName, style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(
+          subcategName,
+          style:
+              const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
       ),
       body: Center(
-        child: Text(mainCategName, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),),
+        child: Text(
+          mainCategName,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        ),
       ),
     );
   }
