1078b1cd323b3474af47ba778f1d513ffa00838a
Nishu2526 <nishikamartin2526@gmail.com>
2022-11-13 10:53:58 +0530

    This is Shopping App..

---
+++ lib/widgets/google_face.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class GoogleFacebookLogin extends StatelessWidget {
+  final String label;
+  final Function() onPressed;
+  final Widget child;
+  const GoogleFacebookLogin(
+      {Key? key,
+      required this.child,
+      required this.label,
+      required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: InkWell(
+        onTap: onPressed,
+        child: Column(
+          children: [
+            SizedBox(height: 50, width: 50, child: child),
+            Text(
+              label,
+              style: const TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
